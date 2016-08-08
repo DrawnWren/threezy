31369bdba93c0342f3332cd834211491c0888477
Hack Reactor Students <accounts@hackreactor.com>
2016-08-07 17:26:51 -0700

    fixing up audio interface

--- client/audio.js
+++ client/audio.js
@@ -1,17 +1,31 @@
-var audioInit = function () {
-  window.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  window.audioElement = document.getElementById('audioElement');
-  window.audioElement.crossOrigin = "anonymous";
-  window.audioSrc = audioCtx.createMediaElementSource(window.audioElement);
+var audioCtx = new (AudioContext || webkitAudioContext)();
+var audioElement = document.getElementById('audioElement');
+audioElement.crossOrigin = "anonymous";
+var audioSrc = audioCtx.createMediaElementSource(audioElement);
 
-  window.analyser = window.audioCtx.createAnalyser();
+var analyser = audioCtx.createAnalyser();
 
-  window.audioSrc.connect(window.analyser);
-  window.audioSrc.connect(window.audioCtx.destination);
+audioSrc.connect(analyser);
+audioSrc.connect(audioCtx.destination);
 
-  window.frequencyArr = new Uint8Array(100);
+var frequencyArr = new Uint8Array(100);
+
+//##FIXME## Function only works for enemies currently,
+//need to refactor to work w/ geometry
+var audioRadius = function (enemArr) {
+  var newE = enemArr.slice();
+  analyser.getByteFrequencyData(frequencyArr);
+  var split =  frequencyArr.length / newE.length; //length of array splits to make
+
+  for (i = 0; i < newE.length; i++) {
+    var s = frequencyArr.slice(i * split, (i + 1) * split);
+    newE[i].radius = freqAvg(s);
+  }
+
+  return newE;
 };
 
 var updateAudioArr = function () {
-  window.analyser.getByteFrequencyData(window.frequencyArr);
+  analyser.getByteFrequencyData(frequencyArr);
 };
+
